7befdc796017382c3887769f376e58a05f6f75de
aashishthakurcodes <j9183208@gmail.com>
2025-01-15 12:46:27 +0530

    Bst

---
+++ BinaryTree/bst.js
@@ -0,0 +1,15 @@
+//Creating node in binary search tree
+class BSTNode {
+    constructor(key){
+      this.key = key;
+      this.left = null
+      this.right = null
+    }
+  }
+  
+//Craeting Tree
+  class BinarySeachTree {
+    constructor(){
+      this.root = null
+    }
+  }  
